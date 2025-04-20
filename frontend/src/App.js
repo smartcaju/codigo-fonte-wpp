0ce443b71f6f77586d344c9742f1d0e023576423
smartcaju <smartcaju@gmail.com>
2025-04-20 16:30:24 -0300

    auteração de logo e cor primaria

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -51,7 +51,7 @@ const App = () => {
             },
             palette: {
                 type: mode,
-                primary: { main: mode === "light" ? "#682EE3" : "#FFFFFF" },
+                primary: { main: mode === "light" ? "#2b2b2b : "#FFFFFF" },
                 textPrimary: mode === "light" ? "#682EE3" : "#FFFFFF",
                 borderPrimary: mode === "light" ? "#682EE3" : "#FFFFFF",
                 dark: { main: mode === "light" ? "#333333" : "#F3F3F3" },
